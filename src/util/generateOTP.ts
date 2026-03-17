5c81f399c2f27f0b03455774cdc387483b3ec1d1
abdullahalkafi-dev <abdullahalkafi742@gmail.com>
2026-03-17 15:38:15 +0600

    feat: enhance OTP generation utility to support customizable length and alphanumeric options

--- src/util/generateOTP.ts
+++ src/util/generateOTP.ts
@@ -1,5 +1,26 @@
-const generateOTP = (): string => {
-  return Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
+import crypto from "crypto";
+
+interface OTPOptions {
+  length?: number;
+  alphanumeric?: boolean;
+}
+
+const generateOTP = (options: OTPOptions = {}): string => {
+  const { length = 6, alphanumeric = false } = options;
+
+  if (alphanumeric) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let otp = "";
+    for (let i = 0; i < length; i++) {
+      otp += chars[crypto.randomInt(0, chars.length)];
+    }
+    return otp;
+  }
+
+  // Generate numeric OTP: 012345
+  const max = Math.pow(10, length);
+  const otp = crypto.randomInt(0, max);
+  return otp.toString().padStart(length, "0");
 };
 
 export default generateOTP;
